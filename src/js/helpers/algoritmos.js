44f850cecfd45d4a711992151d21e037030e348d
Netroid2000 <triple-h-vs-edch@live.com.mx>
2021-05-22 18:51:47 -0600

    primer push

---
+++ src/js/helpers/algoritmos.js
@@ -0,0 +1,89 @@
+function PuntoPendiente(x1, y1, x2, y2){
+    let x = x1,
+      y = y1,
+      stepX = 1,
+      stepY = 1;
+    const dx = x2 - x1;
+    const dy = y2 - y1;
+  
+    if (dx == 0) {
+      if (dy < 0) stepY = -1;
+      while (y !== y2) {
+        point(x, y);
+        y += stepY;
+      }
+    } else {
+      const m = dy / dx;
+      const b = y1 - m * x1;
+      if (dx < 0) stepX = -1;
+      while (x !== x2) {
+        point(x, y);
+        x += stepX;
+        y = m * x + b;
+      }
+    }
+}
+
+function DDA(x1, y1, x2, y2){
+  let x = x1,
+    y = y1,
+    dx = x2 - x1,
+    dy = y2 - y1;
+
+  const m = Math.abs(dx) > Math.abs(dy) ? Math.abs(dx) : Math.abs(dy);
+  const xIncrement = dx / m;
+  const yIncrement = dy / m;
+
+  for (let i = 0; i < m; i++) {
+    x += xIncrement;
+    y += yIncrement;
+    point(x, y);
+  }
+}
+  
+  
+    
+function Bresenham(x1, y1, x2, y2){
+  let x = x1;
+    let y = y1;
+    let dx = x2 - x1;
+    let dy = y2 - y1;
+    let sx = 1;
+    let sy = 1;
+
+    if (dy < 0) {
+      dy = -dy;
+      sy = -1;
+    }
+    if (dx < 0) {
+      dx = -dx;
+      sx = -1;
+    }
+
+    if (dx > dy) {
+      let p = 2 * dy - dx;
+      while (x != x2) {
+        point(x, y);
+        x += sx;
+        if (p < 0) {
+          p += 2 * dy;
+        } else {
+          y += sy;
+          p += 2 * (dy - dx);
+        }
+      }
+    } else {
+      let p = 2 * dx - dy;
+      while (y != y2) {
+        point(x, y);
+        y += sy;
+        if (p < 0) {
+          p += 2 * dx;
+        } else {
+          x += sx;
+          p += 2 * (dx - dy);
+        }
+      }
+    }
+}
+  
---
+++ src/js/sketch.js
@@ -0,0 +1,51 @@
+const radius = 100;
+const algorithms = {
+  pp: "PuntoPendiente",
+  dda: "DDA",
+  bresenham: "Bresenham",
+};
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+}
+
+function draw() {
+  pintarCirculo(algorithms.pp, algorithms.pp, 1);
+  pintarCirculo(algorithms.dda, algorithms.dda, 2);
+  pintarCirculo(algorithms.bresenham, algorithms.bresenham, 3);
+  noLoop();
+}
+
+function pintarCirculo( nombre, accion, pos){
+    const x = (width / 4) * (pos);
+    const y = 200;
+    push();
+    fill('white');
+    circle(x, y, radius * 2);
+    pop();
+    textSize(20);
+    text(nombre, x - radius / 2, y + radius * 2);
+
+    this[accion]( x - radius, y, x + radius, y );
+    this[accion]( x, y - radius, x, y + radius );
+
+    let rad1 = radians(45);
+    let rad2 = radians(225);
+
+    this[accion](
+      Math.floor(x + radius * cos(rad2)),
+      Math.floor(y + radius * sin(rad2)),
+      Math.floor(x + radius * cos(rad1)),
+      Math.floor(y + radius * sin(rad1)),
+    );
+
+    rad1 = radians(135);
+    rad2 = radians(315);
+
+    this[accion](
+      Math.floor(x + radius * cos(rad2)),
+      Math.floor(y + radius * sin(rad2)),
+      Math.floor(x + radius * cos(rad1)),
+      Math.floor(y + radius * sin(rad1)),
+    );
+};
